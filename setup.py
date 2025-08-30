d76537d004e2e42e5b210b119817b5cc7c0eb80c
Dev Tiwari <tiwaridev195@gmail.com>
2025-08-30 21:35:34 +0530

    setup file added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot',
+    version= '0.0.0',
+    author= 'Dev Tiwari',
+    author_email= 'tiwaridev195@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
