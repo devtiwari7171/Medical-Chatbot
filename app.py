14c42dd8af83efc00159cc145bf172ee9260c6fe
Dev Tiwari <tiwaridev195@gmail.com>
2025-08-31 00:19:08 +0530

    Templates added

---
+++ app.py
@@ -0,0 +1,63 @@
+from flask import Flask, render_template, jsonify, request
+from src.helper import download_hugging_face_embeddings
+from langchain_community.vectorstores import Pinecone
+from pinecone import Pinecone, ServerlessSpec
+from langchain_pinecone import PineconeVectorStore
+from langchain.prompts import PromptTemplate
+from langchain_community.llms import CTransformers
+from langchain.chains import RetrievalQA
+from dotenv import load_dotenv
+from src.prompt import *
+import os
+
+app = Flask(__name__)
+
+load_dotenv()
+
+PINECONE_API_KEY = os.environ.get('PINECONE_API_KEY') 
+
+embeddings = download_hugging_face_embeddings()
+
+# Initialize Pinecone (serverless)
+pc = Pinecone(api_key=PINECONE_API_KEY)
+
+INDEX_NAME = "medicalchatbot"
+# Connect to your index using host
+if INDEX_NAME not in pc.list_indexes().names():
+    pc.create_index(
+        name=INDEX_NAME,
+        dimension=384,
+        metric="cosine",
+        spec=ServerlessSpec(cloud="aws", region="us-east-1")
+    )
+
+#Loading the index
+docsearch=PineconeVectorStore.from_existing_index(INDEX_NAME, embeddings)
+
+PROMPT=PromptTemplate(template=prompt_template, input_variables=["context", "question"])
+chain_type_kwargs={"prompt": PROMPT}
+
+llm=CTransformers(model="model/llama-2-7b-chat.ggmlv3.q4_0.bin",
+                  model_type="llama",
+                  config={'max_new_tokens':512,
+                          'temperature':0.8})
+
+
+from langchain_core.vectorstores import VectorStoreRetriever
+
+retriever = VectorStoreRetriever(vectorstore=docsearch, search_kwargs={"k": 2})
+
+qa = RetrievalQA.from_chain_type(
+    llm=llm,
+    chain_type="stuff",
+    retriever=retriever,
+    return_source_documents=True,
+    chain_type_kwargs=chain_type_kwargs
+)
+
+@app.route("/")
+def index():
+    return render_template('chat.html')
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port= 8080, debug= True)
